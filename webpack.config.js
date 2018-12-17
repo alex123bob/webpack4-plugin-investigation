6f32ebb910193ec632f9d54d8147a3c029ec5aac
alex123bob <alex123bob@126.com>
2018-12-17 18:12:11 +0800

    add sourcemap for better debugging

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: 'eval-source-map',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
