8005e7805fb35a45e285d4bb73fee2ace399cbec
alex123bob <alex123bob@126.com>
2018-12-25 18:03:47 +0800

    go presentational component. hells life cycle

--- src/js/component/presentational/Input.js
+++ src/js/component/presentational/Input.js
@@ -2,34 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {InputContext} from '../context/InputContext'
 
-// const Input = ({label, text, type, id, value, handleChange}) => (
-//     <div className="form-group">
-//         <label htmlFor={label}>{text}</label>
-//         <input
-//             type={type}
-//             className="form-control"
-//             id={id}
-//             value={value}
-//             onChange={handleChange}
-//             required
-//         />
-//     </div>
-// )
-
-class Input extends React.Component {
-    static getDerivedStateFromProps(nextProps, prevState) {
-        return prevState
-    }
-
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        const {label, text, type, id, value, handleChange} = this.props
-        return (
-            <InputContext.Consumer>
-            {context => (
+const Input = ({label, text, type, id, value, handleChange}) => (
+    <InputContext.Consumer>
+        {
+            context => (
                 <div className="form-group" style={{backgroundColor: context.backgroundColor}}>
                     <label htmlFor={label}>{text}</label>
                     <input
@@ -41,19 +17,49 @@ class Input extends React.Component {
                         required
                     />
                 </div>
-            )}
-            </InputContext.Consumer>
-        )
-    }
-}
+            )
+        }
+    </InputContext.Consumer>
+)
 
-Input.propTypes = {
-    label: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    handleChange: PropTypes.func.isRequired
-}
+// class Input extends React.Component {
+//     static getDerivedStateFromProps(nextProps, prevState) {
+//         return prevState
+//     }
+
+//     constructor(props) {
+//         super(props)
+//     }
+
+//     render() {
+//         const {label, text, type, id, value, handleChange} = this.props
+//         return (
+//             <InputContext.Consumer>
+//             {context => (
+//                 <div className="form-group" style={{backgroundColor: context.backgroundColor}}>
+//                     <label htmlFor={label}>{text}</label>
+//                     <input
+//                         type={type}
+//                         className="form-control"
+//                         id={id}
+//                         value={value}
+//                         onChange={handleChange}
+//                         required
+//                     />
+//                 </div>
+//             )}
+//             </InputContext.Consumer>
+//         )
+//     }
+// }
+
+// Input.propTypes = {
+//     label: PropTypes.string.isRequired,
+//     text: PropTypes.string.isRequired,
+//     type: PropTypes.string.isRequired,
+//     id: PropTypes.string.isRequired,
+//     value: PropTypes.string.isRequired,
+//     handleChange: PropTypes.func.isRequired
+// }
 
 export default Input
